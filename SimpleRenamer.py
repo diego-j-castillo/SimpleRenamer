9e8948c023552e1b29eb2bd95d47597599c7fbce
Diego <totitodjc@gmail.com>
2023-01-25 11:41:51 -0500

    First functional version

---
+++ SimpleRenamer.py
@@ -0,0 +1,68 @@
+import argparse
+import os
+import tkinter as tk
+from PIL import Image, ImageTk
+
+#parsing arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('path')
+parser.add_argument('-f', '--filter', type=str)
+args = parser.parse_args()
+
+#filter imagelist
+imagelist = []
+templist = []
+if args.filter is not None:
+    for file in os.listdir(args.path):
+        if args.filter.lower() in file.lower():
+            templist.append(file)
+else:
+    templist = os.listdir(args.path)
+for file in templist:
+    if file.endswith('.png') or file.endswith('.jpg'):
+        imagelist.append(file)
+
+#handles pressing enter
+def enterpress(event):
+    global index
+    extension = imagelist[index][imagelist[index].rindex('.'):]
+    if namer.get() != '':
+        try:
+            os.rename(
+                args.path + '/' + imagelist[index],
+                args.path + '/' + namer.get() + extension)
+        except:
+            print('file already exists')
+            return
+    if index == len(imagelist) - 1:
+        window.destroy()
+    else:
+        index = index + 1
+        image = Image.open(args.path + '/' + imagelist[index])
+        global dispimage
+        dispimage = ImageTk.PhotoImage(
+            image.resize((int(image.width * 500 / image.height), 500)))
+        imagelabel.configure(image=dispimage)
+        prevlabel.configure(text=imagelist[index])
+        namer.delete(0, tk.END)
+
+#handles pressing esc by closing window
+def escpress(event):
+    window.destroy()
+
+#starting the GUI
+index = 0
+window = tk.Tk(className='Renamer')
+prevlabel = tk.Label(text=imagelist[index], font='Courier 20')
+image = Image.open(args.path + '/' + imagelist[index])
+dispimage = ImageTk.PhotoImage(
+    image.resize((int(image.width * 500 / image.height), 500)))
+imagelabel = tk.Label(image=dispimage)
+namer = tk.Entry(window, width=20, font='Courier 20')
+prevlabel.pack()
+imagelabel.pack()
+namer.pack()
+namer.focus()
+window.bind('<Return>', enterpress)
+window.bind('<Escape>', escpress)
+window.mainloop()
